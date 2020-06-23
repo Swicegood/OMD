1275fc437e800c6619b4b06613775769d2539a15
Jaga <Jaga@DESKTOP-UHCTFMH>
2020-06-23 13:14:14 -0400

    fix hostname is  not FQDN every docker start

--- dockerscriptetchosts.sh
+++ dockerscriptetchosts.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
-if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
-    touch $CONTAINER_ALREADY_STARTED
-    echo "-- First container startup --"
+line=$(grep -m 1 192.168 /etc/hosts)
+        line2=$(echo $line | awk '{print $2}')
 
-    # YOUR_JUST_ONCE_LOGIC_HERE
- 
-	line=$(grep 192.168 /etc/hosts)
-	line2=$(echo $line | awk '{print $2}')
- 
-	echo "$line $line2.localdomain" >> /etc/hosts
+        echo "$line $line2.localdomain" >> /etc/hosts
 
-	/etc/init.d/sendmail reload
-
-else
-    echo "-- Not first container startup --"
-fi
+        /etc/init.d/sendmail reload
